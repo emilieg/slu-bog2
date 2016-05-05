cccc4f1a6eb47fb1b7845f0720149fdd0f7318e2
emilieg <emilie.gerber@hotmail.com>
2016-05-05 11:19:39 -0700

    committing

---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,38 @@
+class CreaturesController < ApplicationController
+  def index
+    @creatures = Creature.all
+  end
+
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+    creature = Creature.create creature_params
+    redirect_to(creature_path(creature))
+    # '/creatures/8'
+  end
+
+  def show
+    @creature = Creature.find params[:id]
+  end
+
+  def edit
+    @creature = Creature.find params[:id]
+  end
+
+  def update
+    c = Creature.find params[:id]
+    c.update creature_params
+    redirect_to creatures_path
+  end
+
+  def destroy
+    Creature.find(params[:id]).delete
+    redirect_to creatures_path
+  end
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
+end
